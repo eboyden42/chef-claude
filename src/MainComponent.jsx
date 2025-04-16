c5e74017329c99f2e01215abbeeb40058a2ea2d5
Eli Boyden <aqu4eb@virginia.edu>
2025-04-16 10:24:41 -0400

    added ready for recipie component

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -22,12 +22,21 @@ export default function MainComponent() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <div className="ingredient-list">
-                <h2>Ingredients:</h2>
-                <ul>
-                {ingredientList}
-                </ul>
-            </div>
+            <section>
+                <div className="ingredient-list">
+                    <h2>Ingredients:</h2>
+                    <ul>
+                        {ingredientList}
+                    </ul>
+                </div>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>
         </main>
         </>
     )
